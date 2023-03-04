9bbfd922e577cfd6a1f3f92c95c3a99eaaf18b34
Dennis Vesterlund <dennisvesterlund@gmail.com>
2023-03-04 16:54:44 +0100

    Restructured project to fix execution

--- cmd/root.go
+++ cmd/root.go
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-package main
+package cmd
 
 import (
 	"fmt"
@@ -91,7 +91,7 @@ func run(cmd *cobra.Command, args []string) {
 	}
 }
 
-func main() {
+func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
